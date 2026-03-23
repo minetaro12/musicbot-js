1d9b8bfe45341974f1b06d4f707f30e9258e0275
minetaro12 <77948036+minetaro12@users.noreply.github.com>
2026-03-23 17:23:43 +0900

    helpコマンドの文言を変更

--- src/handlers/help.ts
+++ src/handlers/help.ts
@@ -4,7 +4,7 @@ import { DEFAULT_MESSAGE_OPTIONS } from "../lib/messageOptions.ts";
 export const helpHandler = (message: OmitPartialGroupDMChannel<Message<boolean>>) => {
   const helpMessage = `\`\`\`!join: ボイスチャンネルに参加します。
 !leave: ボイスチャンネルから退出します。
-!play <URL>: 指定したURLの音楽を再生します。
+!play <URL|検索ワード>: 指定したURLまたは検索ワードの音楽を再生します。
 !list <ページ番号>: 再生キューの一覧を表示します。
 !skip <スキップ数>: 現在再生中の曲をスキップします。
 !help: このヘルプメッセージを表示します。
